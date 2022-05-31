ef6dd9b87bb8330ee703348ac1d4f8d8fb9d35f8
wowus94 <sesila94@mail.ru>
2022-05-31 20:52:02 +0300

    ДЗ 1: добавили BottomAppBar+FAB

---
+++ app/src/main/java/com/example/mypictureoftheday/view/picture/BottomNavigationDrawerFragment.kt
@@ -0,0 +1,51 @@
+package com.example.mypictureoftheday.view.picture
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mypictureoftheday.R
+import com.example.mypictureoftheday.databinding.BottomNavigationLayoutBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
+
+    private var _binding: BottomNavigationLayoutBinding? = null
+    val binding: BottomNavigationLayoutBinding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = BottomNavigationLayoutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.navigationView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_one -> {
+                    Log.d("@@@", "На экран 1")
+                }
+                R.id.navigation_two -> {
+                    Log.d("@@@", "На экран 2")
+                }
+            }
+            true
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = BottomNavigationDrawerFragment()
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+}
--- app/src/main/java/com/example/mypictureoftheday/view/picture/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/example/mypictureoftheday/view/picture/PictureOfTheDayFragment.kt
@@ -3,15 +3,15 @@ package com.example.mypictureoftheday.view.picture
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import coil.load
 import com.example.mypictureoftheday.R
 import com.example.mypictureoftheday.databinding.FragmentPictureOfTheDayBinding
+import com.example.mypictureoftheday.view.MainActivity
 import com.example.mypictureoftheday.viewmodel.PictureOfTheDayAppState
 import com.example.mypictureoftheday.viewmodel.PictureOfTheDayViewModel
 import java.text.SimpleDateFormat
@@ -31,73 +31,98 @@ class PictureOfTheDayFragment : Fragment() {
         return binding.root
     }
 
-    val viewModel: PictureOfTheDayViewModel by lazy {
+    private val viewModel: PictureOfTheDayViewModel by lazy {
         ViewModelProvider(this).get(PictureOfTheDayViewModel::class.java)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_bottom_bar, menu)
+    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        viewModel.getLiveData().observe(viewLifecycleOwner, Observer {
-            renderData(it)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.app_bar_fav -> {
+                Log.d("@@@", "app_bar_fav")
+            }
+            R.id.app_bar_settings -> {
+                Log.d("@@@", "app_bar_settings")
+            }
+            android.R.id.home -> {
+                BottomNavigationDrawerFragment.newInstance()
+                    .show(requireActivity().supportFragmentManager, "")
+            }
+        }
+        return super.onOptionsItemSelected(item)
+}
+
+
+override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    viewModel.getLiveData().observe(viewLifecycleOwner, Observer {
+        renderData(it)
+    })
+    viewModel.sendServerRequest()
+
+    binding.inputLayout.setEndIconOnClickListener {
+        startActivity(Intent(Intent.ACTION_VIEW).apply {
+            data =
+                Uri.parse("https://en.wikipedia.org/wiki/${binding.inputEditText.text.toString()}")
         })
-        viewModel.sendServerRequest()
-
-        binding.inputLayout.setEndIconOnClickListener {
-            startActivity(Intent(Intent.ACTION_VIEW).apply {
-                data =
-                    Uri.parse("https://en.wikipedia.org/wiki/${binding.inputEditText.text.toString()}")
-            })
-        }
+    }
 
 
-        binding.chipGroup.setOnCheckedChangeListener { group, checkedId ->
-            when (checkedId) {
-                R.id.yesterday -> {
-                    viewModel.sendServerRequest(takeDate(-1))
-                }
-                R.id.today -> {
-                    viewModel.sendServerRequest()
-                }
+    binding.chipGroup.setOnCheckedChangeListener { group, checkedId ->
+        when (checkedId) {
+            R.id.yesterday -> {
+                viewModel.sendServerRequest(takeDate(-1))
+            }
+            R.id.today -> {
+                viewModel.sendServerRequest()
             }
         }
     }
 
-    private fun takeDate(count: Int): String {
-        val currentDate = Calendar.getInstance()
-        currentDate.add(Calendar.DAY_OF_MONTH, count)
-        val format1 = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-        format1.timeZone = TimeZone.getTimeZone("EST")
-        return format1.format(currentDate.time)
-    }
+    (requireActivity() as MainActivity).setSupportActionBar(binding.bottomAppBar)
+    setHasOptionsMenu(true)
+
+}
+
+private fun takeDate(count: Int): String {
+    val currentDate = Calendar.getInstance()
+    currentDate.add(Calendar.DAY_OF_MONTH, count)
+    val format1 = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+    format1.timeZone = TimeZone.getTimeZone("EST")
+    return format1.format(currentDate.time)
+}
 
 
-    private fun renderData(pictureOfTheDayAppState: PictureOfTheDayAppState) {
-        when (pictureOfTheDayAppState) {
-            is PictureOfTheDayAppState.Error -> {
-                // TODO HW
-            }
-            is PictureOfTheDayAppState.Loading -> {
-                binding.imageView.load(R.drawable.progress_animation) {
-                    placeholder(R.drawable.progress_image)
-                    error(R.drawable.ic_load_error_vector)
-                }
-            }
-            is PictureOfTheDayAppState.Success -> {
-                binding.imageView.load(pictureOfTheDayAppState.pictureOfTheDayResponseData.url)
-
+private fun renderData(pictureOfTheDayAppState: PictureOfTheDayAppState) {
+    when (pictureOfTheDayAppState) {
+        is PictureOfTheDayAppState.Error -> {
+            // TODO HW
+        }
+        is PictureOfTheDayAppState.Loading -> {
+            binding.imageView.load(R.drawable.progress_animation) {
+                placeholder(R.drawable.progress_image)
+                error(R.drawable.ic_load_error_vector)
             }
         }
-    }
+        is PictureOfTheDayAppState.Success -> {
+            binding.imageView.load(pictureOfTheDayAppState.pictureOfTheDayResponseData.url)
 
-    companion object {
-        @JvmStatic
-        fun newInstance() = PictureOfTheDayFragment()
+        }
     }
+}
 
-    override fun onDestroy() {
-        _binding = null
-        super.onDestroy()
-    }
+companion object {
+    @JvmStatic
+    fun newInstance() = PictureOfTheDayFragment()
+}
+
+override fun onDestroy() {
+    _binding = null
+    super.onDestroy()
+}
 }
 
