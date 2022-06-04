8bad735831ee2a91f35a50611b455069edb2fa76
Владимир Ляшук <89472439+wowus94@users.noreply.github.com>
2022-06-04 11:29:40 +0300

    ДЗ 1 : решил проблему с фоном

--- app/src/main/java/com/example/mypictureoftheday/view/MainActivity.kt
+++ app/src/main/java/com/example/mypictureoftheday/view/MainActivity.kt
@@ -8,7 +8,7 @@ import com.example.mypictureoftheday.view.picture.PictureOfTheDayFragment
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setTheme(R.style.MyGreenTheme)
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
