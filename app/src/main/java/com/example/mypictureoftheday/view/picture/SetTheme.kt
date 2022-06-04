c7f04a2de518917ab61a7cc638a0e84f8dba43ad
Владимир Ляшук <89472439+wowus94@users.noreply.github.com>
2022-06-04 23:03:23 +0300

    ДЗ 1 : Изменил положение onClick

--- app/src/main/java/com/example/mypictureoftheday/view/picture/SetTheme.kt
+++ app/src/main/java/com/example/mypictureoftheday/view/picture/SetTheme.kt
@@ -9,7 +9,6 @@ import androidx.fragment.app.Fragment
 import com.example.mypictureoftheday.R
 import com.example.mypictureoftheday.databinding.FragmentPictureOfTheDayBinding
 import com.example.mypictureoftheday.view.MainActivity
-import java.lang.reflect.Array.get
 
 class SetTheme : Fragment(), View.OnClickListener {
 
@@ -17,35 +16,35 @@ class SetTheme : Fragment(), View.OnClickListener {
     override fun onAttach(context: Context) {
         super.onAttach(context)
         parentActivity = (context as MainActivity)
+    }
 
-        var _binding: FragmentPictureOfTheDayBinding? = null
-        val binding: FragmentPictureOfTheDayBinding
+    var _binding: FragmentPictureOfTheDayBinding? = null
+    val binding: FragmentPictureOfTheDayBinding
         get() = _binding!!
 
-        override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View? {
-            _binding = FragmentPictureOfTheDayBinding.inflate(inflater, container, false)
-            return binding.root
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPictureOfTheDayBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.setThemeButton.setOnClickListener(this)
+        when (parentActivity.getCurrentTheme()) {
+            1 -> binding.container.(R.id.setThemeButton)
+
         }
 
-        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-            super.onViewCreated(view, savedInstanceState)
-            binding.setThemeButton.setOnClickListener(this)
-            when (parentActivity.getCurrentTheme()) {
-                1 -> binding.container.(R.id.setThemeButton)
+    }
 
-            }
-
-
-            override fun onClick(v: View?) {
-                when (v.id) {
-                    R.id.setThemeButton -> {
-                        parentActivity.setCurrentTheme(RedTheme)
-                        parentActivity.recreate() // применяем для всей активити и для всех дочерних фрагментов
-                    }
-                }
+    override fun onClick(v: View?) {
+        when (v.id) {
+            R.id.setThemeButton -> {
+                parentActivity.setCurrentTheme(RedTheme)
+                parentActivity.recreate() // применяем для всей активити и для всех дочерних фрагментов
             }
         }
     }
